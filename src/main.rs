6ad0ca0802a8b1476cc3e011cf06a9e05d2d877e
Jackson Lewis <st.japa6@gmail.com>
2018-02-21 12:50:40 -0800

    Implemented basic web serving and WS handling.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,26 @@
 #![plugin(rocket_codegen)]
 extern crate rocket;
 mod websocket;
+use std::fs::File;
+use std::io::Read;
+use rocket::response::content::Html;
+use std::thread;
 
 
 #[get("/")]
-fn return_page() -> String {
-    "".to_string()
+fn return_page() -> Html<String> {
+    let mut index = File::open("index.html").unwrap();
+    let mut f_str = String::new();
+    index.read_to_string(&mut f_str).unwrap();
+    Html(f_str)
 }
 
 
 fn main() {
-
+    let server_thread = thread::spawn( || {
+        rocket::ignite().mount("/", routes![return_page]).launch();
+    });
+    let mut socket = websocket::WebSocket::new("127.0.0.1:1234");
+    socket.start();
+    loop {}
 }
--- src/websocket.rs
+++ src/websocket.rs
@@ -1,2 +1,22 @@
 extern crate ws;
-struct WebSocket {}
+use self::ws::listen;
+pub struct WebSocket {
+    ws_path: &'static str
+}
+
+impl WebSocket {
+    pub fn new(path: &'static str) -> WebSocket {
+        WebSocket {
+            ws_path: path
+        }
+    }
+    pub fn start(&mut self) {
+        listen(self.ws_path,
+        |out| {
+            move |msg| {
+                println!("Server got {}",msg);
+                out.send("")
+            }
+        }).unwrap();
+    }
+}
